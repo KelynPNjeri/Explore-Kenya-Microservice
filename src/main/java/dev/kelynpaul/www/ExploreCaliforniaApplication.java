73d498052edc6d63dd17feed6f9b2698f9cb77f5
Kelyn Njeri <kelynpaul20.kp@gmail.com>
2020-10-09 02:59:33 +0300

    Create Spring boilerplate for Microservice.

---
+++ src/main/java/dev/kelynpaul/www/ExploreCaliforniaApplication.java
@@ -0,0 +1,13 @@
+package dev.kelynpaul.www;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExploreCaliforniaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExploreCaliforniaApplication.class, args);
+	}
+
+}
