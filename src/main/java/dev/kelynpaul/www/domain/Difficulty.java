938c218d7ad432e6f7628410b6f247b24ee54ca8
Kelyn Njeri <kelynpaul20.kp@gmail.com>
2020-10-09 03:43:20 +0300

    Add data model to the microservice.

---
+++ src/main/java/dev/kelynpaul/www/domain/Difficulty.java
@@ -0,0 +1,14 @@
+package dev.kelynpaul.www.domain;
+
+/**
+ * Created by maryellenbowman on 7/3/16.
+ */
+
+/**
+ * Enumeration of the level of effort.
+ *
+ * Created by Mary Ellen Bowman
+ */
+public enum Difficulty {
+    Easy, Medium, Difficult, Varies;
+}
