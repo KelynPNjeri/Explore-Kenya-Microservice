5b8f644a7bdeafd5eb1e360216c3f24b8d7b703d
Kelyn Njeri <kelynpaul20.kp@gmail.com>
2020-10-09 03:51:13 +0300

    Add data repos.

---
+++ src/main/java/dev/kelynpaul/www/repo/TourPackageRepository.java
@@ -0,0 +1,7 @@
+package dev.kelynpaul.www.repo;
+
+import dev.kelynpaul.www.domain.TourPackage;
+import org.springframework.data.repository.CrudRepository;
+
+public class TourPackageRepository extends CrudRepository<TourPackage, String> {
+}
---
+++ src/main/java/dev/kelynpaul/www/repo/TourRepository.java
@@ -0,0 +1,8 @@
+package dev.kelynpaul.www.repo;
+
+import dev.kelynpaul.www.domain.Tour;
+import org.springframework.data.repository.CrudRepository;
+
+public interface TourRepository extends CrudRepository<Tour, Integer> {
+
+}
